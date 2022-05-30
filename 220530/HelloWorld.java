b57f5ecb6c17cc535a635c33c1ea457ff4ce6743
Jeon Minji <mjjeon2645@gmail.com>
2022-05-31 01:05:11 +0900

    220530-helloworld-modified
    modified file 220530-helloworld-modified
    
    modified file

--- 220530/HelloWorld.java
+++ 220530/HelloWorld.java
@@ -1,2 +1,17 @@
+// 1. 기존의 방식대로 "Hello, world!"를 출력해보았음.
+// 2. Swing의 개념을 배우고 window 창을 띄워 동일한 문구를 출력해보았음.
+
+import javax.swing.*;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello, World!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        JLabel label = new JLabel("Hello, World!");
+        frame.add(label);
+
+        frame.setVisible(true);
+    }
 }
